6c21ac6bf44b4fad51fd9cce29e78dadcc49bdce
Julian Dobmann <julian.dobmann@mailbox.org>
2022-04-21 11:41:02 +0200

    Querying all images of a single ingredient

--- lib/dujudu/wikidata/client.ex
+++ lib/dujudu/wikidata/client.ex
@@ -1,13 +1,24 @@
 defmodule Dujudu.Wikidata.Client do
   use Tesla
 
-  @ingredients_query File.read!("lib/dujudu/wikidata/ingredients.sparql")
+  @ingredients File.read!("lib/dujudu/wikidata/queries/ingredients.sparql")
+  @ingredient_images File.read!("lib/dujudu/wikidata/queries/ingredient_images.sparql.heex")
 
   plug Tesla.Middleware.BaseUrl, "https://query.wikidata.org"
   plug Tesla.Middleware.Headers, [{"accept", "application/sparql-results+json"}]
 
   def get_ingredients do
-    with {:ok, response} <- get("/sparql", query: [query: @ingredients_query]) do
+    get_response(@ingredients)
+  end
+
+  def get_ingredient_images(wikidata_id) do
+    @ingredient_images
+    |> EEx.eval_string(assigns: [wikidata_id: wikidata_id])
+    |> get_response()
+  end
+
+  defp get_response(query) do
+    with {:ok, response} <- get("/sparql", query: [query: query]) do
       {:ok, unpack_response(response.body)}
     end
   end
--- test/dujudu/wikidata/client_test.exs
+++ test/dujudu/wikidata/client_test.exs
@@ -1,8 +1,9 @@
 defmodule Dujudu.Wikidata.ClientTest do
   use ExUnit.Case
 
-  @ingredients_query File.read!("lib/dujudu/wikidata/ingredients.sparql")
-  @sample_response File.read!("test/dujudu/wikidata/sample_response.json")
+  @ingredients_query File.read!("lib/dujudu/wikidata/queries/ingredients.sparql")
+  @sample_response File.read!("test/dujudu/wikidata/sample_ingredients.json")
+
   @expected_ingredients [
     %{item: %{type: "uri", value: "http://www.wikidata.org/entity/Q81"}, itemLabel: %{type: "literal", value: "carrot", "xml:lang": "en"}},
     %{item: %{type: "uri", value: "http://www.wikidata.org/entity/Q7533"}, itemLabel: %{type: "literal", value: "zucchini", "xml:lang": "en"}},
@@ -27,6 +28,35 @@ defmodule Dujudu.Wikidata.ClientTest do
 
     test "retrieves the current list of ingredients and unpacks response" do
       assert {:ok, @expected_ingredients} == get_ingredients()
+    end
+  end
+
+  @ingredient_id "Q1548030"
+  @ingredient_images_query File.read!("test/dujudu/wikidata/sample_ingredient_images.sparql")
+  @ingredient_images_response File.read!("test/dujudu/wikidata/sample_ingredient_images.json")
+  @expected_images [
+    %{image: %{type: "uri", value: "http://commons.wikimedia.org/wiki/Special:FilePath/Red%20capsicum%20and%20cross%20section.jpg"}},
+    %{image: %{type: "uri", value: "http://commons.wikimedia.org/wiki/Special:FilePath/RedBellPepper.jpg"}}
+  ]
+
+  describe "get_ingredient_images/1" do
+    import Dujudu.Wikidata.Client, only: [get_ingredient_images: 1]
+
+    setup do
+      Tesla.Mock.mock(fn %{
+        method: :get,
+        url: "https://query.wikidata.org/sparql",
+        headers: [{"accept", "application/sparql-results+json"}],
+        query: [query: @ingredient_images_query]
+      } ->
+        %Tesla.Env{status: 200, body: @ingredient_images_response}
+      end)
+
+      :ok
+    end
+
+    test "retrieves images for the given ingredient and unpacks response" do
+      assert {:ok, @expected_images} == get_ingredient_images(@ingredient_id)
     end
   end
 end
