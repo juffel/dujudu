57b0716bf3c3c1a9b466778964beefe39e77063d
Julian Dobmann <julian.dobmann@mailbox.org>
2022-04-26 13:51:38 +0200

    Remove unused generated bulk code

--- lib/dujudu_web/controllers/ingredient_controller.ex
+++ lib/dujudu_web/controllers/ingredient_controller.ex
@@ -1,7 +1,6 @@
 defmodule DujuduWeb.IngredientController do
   use DujuduWeb, :controller
 
-  alias Dujudu.Models
   alias Dujudu.Access.Ingredients
   alias Dujudu.Schemas.Ingredient
 
@@ -12,49 +11,8 @@ defmodule DujuduWeb.IngredientController do
     end
   end
 
-  def create(conn, %{"ingredient" => ingredient_params}) do
-    case Models.create_ingredient(ingredient_params) do
-      {:ok, ingredient} ->
-        conn
-        |> put_flash(:info, "Ingredient created successfully.")
-        |> redirect(to: Routes.ingredient_path(conn, :show, ingredient))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
-
   def show(conn, %{"id" => id}) do
     ingredient = Ingredients.get_ingredient(id)
     render(conn, "show.html", ingredient: ingredient)
-  end
-
-  def edit(conn, %{"id" => id}) do
-    ingredient = Models.get_ingredient!(id)
-    changeset = Models.change_ingredient(ingredient)
-    render(conn, "edit.html", ingredient: ingredient, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "ingredient" => ingredient_params}) do
-    ingredient = Models.get_ingredient!(id)
-
-    case Models.update_ingredient(ingredient, ingredient_params) do
-      {:ok, ingredient} ->
-        conn
-        |> put_flash(:info, "Ingredient updated successfully.")
-        |> redirect(to: Routes.ingredient_path(conn, :show, ingredient))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", ingredient: ingredient, changeset: changeset)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    ingredient = Models.get_ingredient!(id)
-    {:ok, _ingredient} = Models.delete_ingredient(ingredient)
-
-    conn
-    |> put_flash(:info, "Ingredient deleted successfully.")
-    |> redirect(to: Routes.ingredient_path(conn, :index))
   end
 end
--- test/dujudu_web/controllers/ingredient_controller_test.exs
+++ test/dujudu_web/controllers/ingredient_controller_test.exs
@@ -1,10 +1,6 @@
 defmodule DujuduWeb.IngredientControllerTest do
   use DujuduWeb.ConnCase
 
-  import Dujudu.ModelsFixtures
-
-  @create_attrs %{name: "some name"}
-  @update_attrs %{name: "some updated name"}
   @invalid_attrs %{name: nil}
 
   describe "index" do
@@ -12,73 +8,5 @@ defmodule DujuduWeb.IngredientControllerTest do
       conn = get(conn, Routes.ingredient_path(conn, :index))
       assert html_response(conn, 200) =~ "Listing Ingredients"
     end
-  end
-
-  describe "new ingredient" do
-    test "renders form", %{conn: conn} do
-      conn = get(conn, Routes.ingredient_path(conn, :new))
-      assert html_response(conn, 200) =~ "New Ingredient"
-    end
-  end
-
-  describe "create ingredient" do
-    test "redirects to show when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.ingredient_path(conn, :create), ingredient: @create_attrs)
-
-      assert %{id: id} = redirected_params(conn)
-      assert redirected_to(conn) == Routes.ingredient_path(conn, :show, id)
-
-      conn = get(conn, Routes.ingredient_path(conn, :show, id))
-      assert html_response(conn, 200) =~ "Show Ingredient"
-    end
-
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.ingredient_path(conn, :create), ingredient: @invalid_attrs)
-      assert html_response(conn, 200) =~ "New Ingredient"
-    end
-  end
-
-  describe "edit ingredient" do
-    setup [:create_ingredient]
-
-    test "renders form for editing chosen ingredient", %{conn: conn, ingredient: ingredient} do
-      conn = get(conn, Routes.ingredient_path(conn, :edit, ingredient))
-      assert html_response(conn, 200) =~ "Edit Ingredient"
-    end
-  end
-
-  describe "update ingredient" do
-    setup [:create_ingredient]
-
-    test "redirects when data is valid", %{conn: conn, ingredient: ingredient} do
-      conn = put(conn, Routes.ingredient_path(conn, :update, ingredient), ingredient: @update_attrs)
-      assert redirected_to(conn) == Routes.ingredient_path(conn, :show, ingredient)
-
-      conn = get(conn, Routes.ingredient_path(conn, :show, ingredient))
-      assert html_response(conn, 200) =~ "some updated name"
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, ingredient: ingredient} do
-      conn = put(conn, Routes.ingredient_path(conn, :update, ingredient), ingredient: @invalid_attrs)
-      assert html_response(conn, 200) =~ "Edit Ingredient"
-    end
-  end
-
-  describe "delete ingredient" do
-    setup [:create_ingredient]
-
-    test "deletes chosen ingredient", %{conn: conn, ingredient: ingredient} do
-      conn = delete(conn, Routes.ingredient_path(conn, :delete, ingredient))
-      assert redirected_to(conn) == Routes.ingredient_path(conn, :index)
-
-      assert_error_sent 404, fn ->
-        get(conn, Routes.ingredient_path(conn, :show, ingredient))
-      end
-    end
-  end
-
-  defp create_ingredient(_) do
-    ingredient = ingredient_fixture()
-    %{ingredient: ingredient}
   end
 end
