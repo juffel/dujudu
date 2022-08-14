20ad7e7509be16b7f0a17fed550ccb70763bd84f
Julian Dobmann <julian.dobmann@mailbox.org>
2022-08-14 15:46:35 +0200

    Align Flop with LiveView

--- lib/dujudu_web/live/ingredient_index_live.ex
+++ lib/dujudu_web/live/ingredient_index_live.ex
@@ -9,28 +9,43 @@ defmodule DujuduWeb.IngredientIndexLive do
 
   on_mount DujuduWeb.Auth.LiveAuth
 
-  def mount(%{"filters" => %{"0" => %{"value" => search}}}, _session, socket) do
-    {:ok, fetch_ingredients(search, socket)}
+  def mount(params, _session, socket) do
+    socket = assign(socket, :params, params)
+    {:ok, fetch_ingredients(socket)}
   end
 
-  def mount(_params, _session, socket) do
-    {:ok, fetch_ingredients(nil, socket)}
-  end
+  def handle_params(params, _uri, socket) do
+    case params["page"] do
+      nil ->
+        {:noreply, socket}
 
-  defp fetch_ingredients(search, socket) do
-    params = %{
-      "filters" => %{
-        "0" => %{"field" => "title", "op" => "=~", "value" => search}
-      }
-    }
+      page ->
+        merged_params = Map.merge(socket.assigns.params, %{"page" => page})
+        updated_socket = assign(socket, :params, merged_params)
 
-    with {:ok, flop} <- Flop.validate(params, for: Ingredient) do
-      {ingredients, meta} = Ingredients.list_ingredients(flop)
-      assign(socket, meta: meta, ingredients: ingredients)
+        {:noreply, fetch_ingredients(updated_socket)}
     end
   end
 
   def handle_event("search", %{"filters" => %{"0" => %{"value" => search}}}, socket) do
-    {:noreply, fetch_ingredients(search, socket)}
+    search_params = %{"0" => %{"field" => "title", "op" => "ilike", "value" => search}}
+    merged_params = Map.merge(socket.assigns.params, %{"filters" => search_params})
+
+    updated_socket = assign(socket, params: merged_params)
+    {:noreply, fetch_ingredients(updated_socket)}
   end
+
+  defp fetch_ingredients(socket) do
+    with {:ok, flop} <- Flop.validate(socket.assigns.params, for: Ingredient) do
+      {ingredients, meta} = Ingredients.list_ingredients(flop)
+      assign(socket, meta: meta, ingredients: ingredients, current_search: get_current_search(meta))
+    end
+  end
+
+  defp get_current_search(%{flop: %{filters: filters}}) do
+    search = Enum.find(filters, fn element -> Map.get(element, :field) == :title end)
+    Map.get(search, :value, nil)
+  end
+
+  defp get_current_search(_), do: nil
 end
