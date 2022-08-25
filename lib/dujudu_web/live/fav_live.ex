b8509cdd6b9cc291dc7e75c2ec740d70328d0e6b
Julian Dobmann <julian.dobmann@mailbox.org>
2022-08-25 14:21:34 +0200

    Fix fav live pagination handling

--- lib/dujudu_web/live/fav_live.ex
+++ lib/dujudu_web/live/fav_live.ex
@@ -8,13 +8,17 @@ defmodule DujuduWeb.FavLive do
 
   on_mount DujuduWeb.Auth.LiveAuth
 
-  def mount(params, _session, socket) do
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, page_title: "Favourites")}
+  end
+
+  def handle_params(params, _uri, socket) do
     %{id: account_id} = socket.assigns[:current_account]
 
     with {:ok, flop} <- Flop.validate(params, for: Ingredient) do
       {ingredients, meta} = Ingredients.list_fav_ingredients(flop, account_id)
 
-      {:ok, assign(socket, meta: meta, ingredients: ingredients, page_title: "Favourites")}
+      {:noreply, assign(socket, meta: meta, ingredients: ingredients, page_title: "Favourites")}
     end
   end
 end
