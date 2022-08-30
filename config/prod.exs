b76cdfacf632ed9ec0d9176cad2bffd51c2f538a
Julian Dobmann <julian.dobmann@mailbox.org>
2022-08-30 13:01:42 +0200

    Update :check_origin value to allow liveview ws connections

--- config/prod.exs
+++ config/prod.exs
@@ -54,6 +54,7 @@ config :dujudu, DujuduWeb.Endpoint,
   http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  check_origin: ["//" <> System.get_env("APP_NAME") <> ".gigalixirapp.com"],
   server: true
 
 config :dujudu, Dujudu.Repo,
