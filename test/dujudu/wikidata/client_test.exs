3df348a85c741b0876c356a6e41d9519b650f107
Julian Dobmann <julian.dobmann@mailbox.org>
2022-09-01 15:27:47 +0200

    Finalize standalone Client + test

--- test/dujudu/wikidata/client_test.exs
+++ test/dujudu/wikidata/client_test.exs
@@ -3,13 +3,10 @@ defmodule Dujudu.Wikidata.ClientTest do
 
   import Dujudu.Wikidata.Client, only: [get_ingredients: 0]
 
-  alias Dujudu.Wikidata.ClientRequest
-  alias Dujudu.Repo
-
   @ingredients_query File.read!("lib/dujudu/wikidata/queries/ingredients.sparql")
   @sample_response File.read!("test/dujudu/wikidata/sample_ingredients.json")
 
-  describe "get_ingredients/0" do
+  describe "get_request/0" do
     setup do
       Tesla.Mock.mock(fn %{
                            method: :get,
@@ -27,30 +24,8 @@ defmodule Dujudu.Wikidata.ClientTest do
       :ok
     end
 
-    test "retrieves the current list of ingredients" do
-      assert {:ok, @sample_response} = get_ingredients()
-    end
-
-    test "stores a record of the latest request" do
-      assert Repo.all(ClientRequest) == []
-      get_ingredients()
-      [request] = Repo.all(ClientRequest)
-      assert request.query == @ingredients_query
-      assert request.response_body == @sample_response
-    end
-
-    @hours_ago_25 DateTime.utc_now() |> DateTime.add(-60 * 60 * 25, :second)
-    @hours_ago_23 DateTime.utc_now() |> DateTime.add(-60 * 60 * 23, :second)
-
-    test "removes outdated records and keeps newer ones" do
-      outdated_record = insert(:wikidata_client_request, inserted_at: @hours_ago_25)
-      recent_record = insert(:wikidata_client_request, inserted_at: @hours_ago_23)
-
-      get_ingredients()
-
-      logged_requests = Repo.all(ClientRequest)
-      refute outdated_record in logged_requests
-      assert recent_record in logged_requests
+    test "queries wikidata sparql api and returns result" do
+      assert {:ok, %{body: @sample_response}} = get_ingredients()
     end
   end
 
@@ -63,7 +38,7 @@ defmodule Dujudu.Wikidata.ClientTest do
 
     test "returns general error when other error occurs" do
       Tesla.Mock.mock(fn %{method: :get} -> {:error, :something_else} end)
-      assert {:error, :wikidata_client_error, {:error, :something_else}} == get_ingredients()
+      assert {:error, :wikidata_client_error, :something_else} == get_ingredients()
     end
   end
 end
