52c94d93980710ee9f13bc4cfd8fd722e17a25b0
Julian Dobmann <julian.dobmann@mailbox.org>
2022-09-02 08:31:32 +0200

    Remove outdated test

--- test/dujudu/wikidata/ingredients_test.exs
+++ test/dujudu/wikidata/ingredients_test.exs
@@ -16,49 +16,4 @@ defmodule Dujudu.Wikidata.IngredientsTest do
       assert Jason.decode!(@expected_ingredient_data, keys: :atoms) == ingredient_data
     end
   end
-
-  setup do
-    Tesla.Mock.mock(fn %{method: :get} ->
-      %Tesla.Env{status: 200, body: @sample_response}
-    end)
-
-    :ok
-  end
-
-  @sample_response File.read!("test/dujudu/wikidata/sample_ingredients.json")
-
-  describe "fetch_cached_ingredients/0" do
-    import Dujudu.Wikidata.Ingredients, only: [fetch_cached_ingredients: 0]
-
-    @expected [
-      %Entity{
-        title: "honey",
-        wikidata_id: "Q10987"
-      },
-      %Entity{
-        title: "zucchini",
-        wikidata_id: "Q7533"
-      },
-      %Entity{
-        title: "carrot",
-        description: "rabbits like 'em",
-        wikidata_id: "Q81",
-        instance_of_wikidata_ids: MapSet.new(["Q25403900"]),
-        subclass_of_wikidata_ids: MapSet.new(["Q10675206", "Q2095"]),
-        commons_image_urls:
-          MapSet.new([
-            "http://commons.wikimedia.org/wiki/Special:FilePath/Another_Carrot_pic.jpg",
-            "http://commons.wikimedia.org/wiki/Special:FilePath/Foo.Bar.02.jpg"
-          ])
-      }
-    ]
-
-    test "calls client and returns a list of maps" do
-      assert @expected == sort(fetch_cached_ingredients())
-    end
-
-    defp sort(ingredients) do
-      Enum.sort_by(ingredients, & &1.wikidata_id)
-    end
-  end
 end
