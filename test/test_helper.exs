b13c3f2293257c5949d5082c073a3f52f3635790
Julian Dobmann <julian.dobmann@mailbox.org>
2022-09-02 13:25:16 +0200

    Re-enable wallaby

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,5 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Dujudu.Repo, :manual)
 
-# TODO: fix docker image for chromedriver
-# {:ok, _} = Application.ensure_all_started(:wallaby)
-# Application.put_env(:wallaby, :base_url, DujuduWeb.Endpoint.url())
+{:ok, _} = Application.ensure_all_started(:wallaby)
+Application.put_env(:wallaby, :base_url, DujuduWeb.Endpoint.url())
