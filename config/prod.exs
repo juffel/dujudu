20994568ac79ad9c3cc3c74792eb829254afee5c
Julian Dobmann <julian.dobmann@mailbox.org>
2022-09-02 19:31:49 +0200

    Strip out unneeded config artifacts

--- config/prod.exs
+++ config/prod.exs
@@ -53,14 +53,7 @@ config :logger, level: :info
 config :dujudu, DujuduWeb.Endpoint,
   # Possibly not needed, but doesn't hurt
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  check_origin: ["//" <> System.get_env("APP_NAME") <> ".gigalixirapp.com"],
+  url: [host: "dujudu.fly.dev", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  check_origin: ["//dujudu.fly.dev"],
   server: true
-
-config :dujudu, Dujudu.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  ssl: true,
-  # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
-  pool_size: 2
