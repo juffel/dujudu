977148f6d8db145ad3db9162475f4f379e3fb2f7
Julian Dobmann <julian.dobmann@mailbox.org>
2022-09-04 13:13:55 +0200

    Setup fly.io deployment 🎈 (#22)
    * Run fly launch
    
    * Use images that are actually available
    
    * Strip out unneeded config artifacts
    
    * Add production env file and see if it help
    
    * Update runtime.exs for once
    
    * Remove last gigalixir artifact
    
    * Read query file at compile time instead of runtime
    
    * Remove remote debugging entries
    
    * Restore original docker-compose setup

--- config/prod.exs
+++ config/prod.exs
@@ -47,20 +47,3 @@ config :logger, level: :info
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
-
-# Copied over from Gigalixir's getting started guide
-# https://gigalixir.readthedocs.io/en/latest/modify-app/mix.html#modifying-existing-app-with-mix
-config :dujudu, DujuduWeb.Endpoint,
-  # Possibly not needed, but doesn't hurt
-  http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  check_origin: ["//" <> System.get_env("APP_NAME") <> ".gigalixirapp.com"],
-  server: true
-
-config :dujudu, Dujudu.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  ssl: true,
-  # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
-  pool_size: 2
--- config/runtime.exs
+++ config/runtime.exs
@@ -43,6 +43,9 @@ if config_env() == :prod do
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
+  live_view_signing_salt =
+    System.get_env("LIVEVIEW_SECRET_SALT") || raise "LIVEVIEW_SECRET_SALT is missing"
+
   config :dujudu, DujuduWeb.Endpoint,
     url: [host: host, port: 443],
     http: [
@@ -53,7 +56,8 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    live_view: [signing_salt: live_view_signing_salt]
 
   # ## Using releases
   #
