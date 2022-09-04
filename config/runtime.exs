086888a230d45337b191d93d82649105f930bfe1
Julian Dobmann <julian.dobmann@mailbox.org>
2022-09-04 13:05:53 +0200

    Format

--- config/runtime.exs
+++ config/runtime.exs
@@ -42,7 +42,9 @@ if config_env() == :prod do
 
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
-  live_view_signing_salt = System.get_env("LIVEVIEW_SECRET_SALT") || raise "LIVEVIEW_SECRET_SALT is missing"
+
+  live_view_signing_salt =
+    System.get_env("LIVEVIEW_SECRET_SALT") || raise "LIVEVIEW_SECRET_SALT is missing"
 
   config :dujudu, DujuduWeb.Endpoint,
     url: [host: host, port: 443],
