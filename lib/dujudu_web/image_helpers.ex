23cc32ce582d0bbdb2bfab5b48f366a554c00602
Julian Dobmann <julian.dobmann@mailbox.org>
2022-09-07 15:40:37 +0200

    Add blank image and align cells

--- lib/dujudu_web/image_helpers.ex
+++ lib/dujudu_web/image_helpers.ex
@@ -1,5 +1,6 @@
 defmodule DujuduWeb.ImageHelpers do
   @wikidata_base_url "https://www.wikidata.org/wiki"
+  @blank_image_path "/images/1x1-00000000.png"
 
   def image_url_for(ingredient) do
     ingredient.commons_image_urls |> List.first()
@@ -7,5 +8,9 @@ defmodule DujuduWeb.ImageHelpers do
 
   def wikidata_url(%{wikidata_id: id}) do
     Path.join(@wikidata_base_url, id)
+  end
+
+  def blank_image_url(socket) do
+    DujuduWeb.Router.Helpers.static_path(socket, @blank_image_path)
   end
 end
