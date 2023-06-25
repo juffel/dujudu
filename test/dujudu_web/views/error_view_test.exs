e57d43398d14079d863dee836d40d50c0d52986e
Julian Dobmann <suppiger-grat.0y@icloud.com>
2023-06-25 21:59:29 +0200

    WIP Fix test

--- test/dujudu_web/views/error_view_test.exs
+++ test/dujudu_web/views/error_view_test.exs
@@ -2,7 +2,7 @@ defmodule DujuduWeb.ErrorViewTest do
   use DujuduWeb.ConnCase, async: true
 
   # Bring render/3 and render_to_string/3 for testing custom views
-  # import Phoenix.View
+  import Phoenix.View, only: [render_to_string: 3]
 
   test "renders 404.html" do
     assert render_to_string(DujuduWeb.ErrorView, "404.html", []) == "Not Found"
