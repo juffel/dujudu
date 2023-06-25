d81556b6a4ef029e08dfcec76a914a26d584a2a0
Julian Dobmann <suppiger-grat.0y@icloud.com>
2023-06-25 19:20:39 +0200

    Update phoenix to 1.7

--- mix.exs
+++ mix.exs
@@ -7,7 +7,6 @@ defmodule Dujudu.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -33,7 +32,7 @@ defmodule Dujudu.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.16"},
+      {:phoenix, "~> 1.7.0"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
@@ -48,7 +47,7 @@ defmodule Dujudu.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
+      {:plug_cowboy, "~> 2.6"},
       {:tesla, "~> 1.4"},
       {:hackney, "~> 1.6"},
       {:guardian, "~> 2.0"},
